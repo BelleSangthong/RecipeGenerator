66b58691a5f17ff92930bd1a743c04ce12c425af
Belle Sangthong <belle.sangthong@iths.se>
2024-10-22 17:59:48 +0200

    failed again

--- CookingRecipe/src/RecipeManager.java
+++ CookingRecipe/src/RecipeManager.java
@@ -1,2 +1,3 @@
-public class RecipeManager {
+public class RecipeManager <T>{
+
 }
--- CookingRecipe/test/RecipeManagementTest.java
+++ CookingRecipe/test/RecipeManagementTest.java
@@ -6,6 +6,11 @@ public class RecipeManagementTest {
     @Test
     public void testAddRecipe() {
         RecipeManager manager = new RecipeManager();
+        Recipe recipe = new Recipe("Microwave Scrambled Eggs",
+                                    "Crack eggs into a bowl, " +
+                                    "whisk with salt and peppar, " +
+                                    "microwave for 1 minute until cooked.");
+
     }
 
 }
