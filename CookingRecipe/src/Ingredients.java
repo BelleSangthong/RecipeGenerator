2ac1bff2a4a163d7754c786c80f6de6fd92a95d7
Belle Sangthong <belle.sangthong@iths.se>
2024-10-22 19:21:16 +0200

    solution: add arguments and create a constructure

--- CookingRecipe/src/Ingredients.java
+++ CookingRecipe/src/Ingredients.java
@@ -1,2 +1,10 @@
 public class Ingredients {
+    private String name;
+    private String quantity;
+
+    //Konstruktor
+    public Ingredients(String name, String quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
 }
