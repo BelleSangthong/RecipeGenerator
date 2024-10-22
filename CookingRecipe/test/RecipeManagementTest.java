6bb7f1bc4231aad486b90978bed14d443915d096
Belle Sangthong <belle.sangthong@iths.se>
2024-10-22 16:51:23 +0200

    My first test failed

--- CookingRecipe/test/RecipeManagementTest.java
+++ CookingRecipe/test/RecipeManagementTest.java
@@ -1,4 +1,11 @@
+import org.junit.jupiter.api.Test;
+
 public class RecipeManagementTest {
 
     //TODO Make a test when a recipe added
+    @Test
+    public void testAddRecipe() {
+        RecipeManager manager = new RecipeManager();
+    }
+
 }
